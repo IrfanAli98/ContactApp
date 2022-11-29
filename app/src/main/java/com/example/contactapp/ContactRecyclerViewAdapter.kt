878dd20ed512963af6a9d830afe1dcf5d1cb1fb9
IrfanAli98 <116127997+IrfanAli98@users.noreply.github.com>
2022-11-29 13:23:05 +0530

    ContactApp

--- app/src/main/java/com/example/contactapp/ContactRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/contactapp/ContactRecyclerViewAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.contactapp
 
+import android.Manifest
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.Intent.*
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
@@ -23,20 +28,29 @@ class ContactRecyclerViewAdapter(val context: Context, private var contactList: 
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         var contacts = contactList.get(position)
+        val requestPermissionList = arrayOf(Manifest.permission.CALL_PHONE)
+        val REQUEST_CODE = 1
+
         holder.layoutCustomViewBinding.tvFname.text = contacts.fname
         holder.layoutCustomViewBinding.tvLname.text = contacts.lname
         holder.layoutCustomViewBinding.tvNumber.text = contacts.mobNo
+        val mobileNo:String = contacts.mobNo
 
         holder.layoutCustomViewBinding.btnDial.setOnClickListener{
-            val mobileNo:String = contacts.mobNo
+
             val intent = Intent(ACTION_DIAL, Uri.parse("tel:$mobileNo"))
             context.startActivity(intent)
         }
-        holder.layoutCustomViewBinding.btnCall.setOnClickListener{
-            val mobileNo:String = contacts.mobNo
-            val intent = Intent(ACTION_CALL, Uri.parse("tel:$mobileNo"))
-            context.startActivity(intent)
 
+        holder.layoutCustomViewBinding.btnCall.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(ACTION_CALL, Uri.parse("tel:$mobileNo"))
+                context.startActivity(intent)
+            }
+            else{
+                ActivityCompat.requestPermissions(context as Activity, requestPermissionList, REQUEST_CODE)
+
+            }
         }
 
         holder.itemView.setOnClickListener {
